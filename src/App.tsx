1941305f925efe80b4b7ce0e01587588ece349f8
Ana Ruiz <ruizana80@gmail.com>
2023-03-18 13:40:40 -0600

    draggable

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import { Text } from './components/atoms';
 import moment from 'moment';
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
-
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import type { RootState } from './store';
 import { Card, AddCard, AddPendingModal, Footer } from './components/organisms';
 import { Box, Grid, ThemeProvider } from '@mui/material';
@@ -14,7 +14,16 @@ function App() {
 	const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 	const [isModalOpen, setIsModalOpen] = useState(false);
 	const list = useAppSelector((state): any => state.pending.pendingsList);
+	const [listPending, setListPending] = useState(list);
 
+	function handleDragOnEnd(result: any) {
+		if (list) {
+			const items = Array.from(list);
+			const [reorderedItem] = items.splice(result.source.index, 1);
+			items.splice(result.destination.index, 0, reorderedItem);
+			setListPending(items);
+		}
+	}
 	return (
 		<ThemeProvider theme={theme}>
 			<Box
@@ -24,22 +33,41 @@ function App() {
 					display: 'flex',
 					flexDirection: 'column',
 				}}>
-				<Grid
-					container
-					spacing={{ xs: 2, md: 3 }}
-					columns={{ xs: 1, sm: 2, md: 4 }}>
-					<AddCard openModal={() => setIsModalOpen(true)} />
-					{list?.map((pending: PendingListProps) => (
-						<Card
-							key={pending.id}
-							id={pending.id}
-							description={pending.description}
-							priority={pending.priority}
-							status={pending.status}
-							time={moment().format('MMMM Do YYYY, h:mm:ss a')}
-						/>
-					))}
-				</Grid>
+				<DragDropContext onDragEnd={handleDragOnEnd}>
+					<Droppable droppableId="pendings">
+						{provided => (
+							<div {...provided.droppableProps} {...provided.innerRef}>
+								<Grid
+									container
+									spacing={{ xs: 2, md: 3 }}
+									columns={{ xs: 1, sm: 2, md: 4 }}>
+									<AddCard openModal={() => setIsModalOpen(true)} />
+									{listPending?.map((pending: any, index: any) => (
+										<Draggable
+											key={pending.id}
+											draggableId={pending.id}
+											index={index}>
+											{(provided): any => (
+												<div
+													ref={provided.innerRef}
+													{...provided.draggableProps}
+													{...provided.dragHandleProps}>
+													<Card
+														id={pending.id}
+														description={pending.description}
+														priority={pending.priority}
+														status={pending.status}
+														time={moment().format('MMMM Do YYYY, h:mm:ss a')}
+													/>
+												</div>
+											)}
+										</Draggable>
+									))}
+								</Grid>
+							</div>
+						)}
+					</Droppable>
+				</DragDropContext>
 				{list.length == 0 && (
 					<Text
 						variant="h2"
